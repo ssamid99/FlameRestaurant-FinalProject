ca873a6b755bec2648c177f5c00dd96f2f19a83d
Samid <samidms@code.edu.az>
2023-02-13 19:02:06 +0400

    Add project files.

---
+++ FlameRestaurant.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FlameRestaurant.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
